556264932ef511f12f4aa13051da402e43600097
Ubercola <cola@cactus.ru>
2023-12-22 15:58:24 +0300

    republish odom2d to 4hz, add tf Broadcaster

--- scripts/simple_odom.py
+++ scripts/simple_odom.py
@@ -6,12 +6,14 @@ import math
 from rclpy.node import Node
 
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose2D
-
+from geometry_msgs.msg import Pose2D, TransformStamped
+from tf2_ros import TransformBroadcaster
 
 ROS_DOMAIN_ID :int = int(os.environ.get('ROS_DOMAIN_ID',0))
 
 class SimpleOdometry(Node):
+
+    msg: Odometry = Odometry()
 
     def __init__(self):
         super().__init__('simple_odom_republish')
@@ -21,6 +23,9 @@ class SimpleOdometry(Node):
                                           depth=1)
 
         self.publisher = self.create_publisher(Pose2D, 'odom2d', 10)
+        self.tf_broadcaster = TransformBroadcaster(self)
+        self.simple_odom_timer = self.create_timer(timer_period_sec = 0.25, 
+                                                   callback = self.simple_odom_publisher)
         
         self.subscription = self.create_subscription(
             Odometry,
@@ -32,13 +37,27 @@ class SimpleOdometry(Node):
 
     def listener_callback(self, msg: Odometry):
 
-        pose2d = Pose2D()
-        pose2d.x = msg.pose.pose.position.x
-        pose2d.y = msg.pose.pose.position.y
-        pose2d.theta = self.quaternion_to_theta(msg)
-        self.publisher.publish(pose2d)
+        self.msg = msg
+
+        t = TransformStamped()
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = 'odom'
+        t.child_frame_id = 'base_footprint'
+        t.transform.translation.x = msg.pose.pose.position.x
+        t.transform.translation.y = msg.pose.pose.position.y
+        t.transform.translation.z = 0.0
+        t.transform.rotation = msg.pose.pose.orientation
+
+        self.tf_broadcaster.sendTransform(t)
 
         self.get_logger().debug('I heard: "%s"' % msg.pose.pose.position.x)
+
+    def simple_odom_publisher(self):
+        pose2d = Pose2D()
+        pose2d.x = self.msg.pose.pose.position.x
+        pose2d.y = self.msg.pose.pose.position.y
+        pose2d.theta = self.quaternion_to_theta(self.msg)
+        self.publisher.publish(pose2d)
 
     def quaternion_to_theta(self, odom: Odometry):
 
@@ -52,11 +71,14 @@ def main(args=None):
 
     rclpy.init(domain_id=ROS_DOMAIN_ID)
 
-    simple_subscriber = SimpleOdometry()
+    node = SimpleOdometry()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass    
 
-    rclpy.spin(simple_subscriber)
-
-    simple_subscriber.destroy_node()
+    node.destroy_node()
+    
     rclpy.shutdown()
 
 
