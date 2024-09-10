07eedd796f20498e4ebc074d07a784840a2aa94c
Ubercola <cola@cactus.ru>
2024-09-10 14:37:54 +0300

    New visual for leds

--- scripts/leds.py
+++ scripts/leds.py
@@ -13,25 +13,28 @@ class LedDemoPublisher(Node):
 
     def __init__(self):
         super().__init__('leddemo_publisher')
+        self.led_msg = ColorRGBAArray()
         self.publisher_ = self.create_publisher(ColorRGBAArray, '/backlight/array', 10)
-        timer_period = 0.05  # seconds
+        timer_period = 0.075  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         
         leds = list(range(24))
-        self.red_iter = cycle(leds[2:] + leds[:2]) # + 2 led cycle
+        self.red_iter = cycle(leds[2:] + leds[:2]) # +2 led cycle
         self.green_iter = cycle(leds[1:] + leds[:1]) # +1 led cycle
         self.blue_iter = cycle(leds)
         
 
     def timer_callback(self):
-        led_msg = ColorRGBAArray()
 
-        led_msg.array[next(self.red_iter)]   = ColorRGBA(r = 1.0, g = 0.0, b = 0.0, a = 0.0)
-        led_msg.array[next(self.green_iter)] = ColorRGBA(r = 0.0, g = 1.0, b = 0.0, a = 0.0)
-        led_msg.array[next(self.blue_iter)]  = ColorRGBA(r = 0.0, g = 0.0, b = 1.0, a = 0.0)
+        for key, msg in enumerate(self.led_msg.array):
+            if msg.b > 0: self.led_msg.array[key].b = msg.b * 0.80
+        
+        self.led_msg.array[next(self.red_iter)]   = ColorRGBA(r = 1.0, g = 0.0, b = 0.0, a = 1.0)
+        self.led_msg.array[next(self.green_iter)] = ColorRGBA(r = 0.0, g = 1.0, b = 0.0, a = 1.0)
+        self.led_msg.array[next(self.blue_iter)]  = ColorRGBA(r = 0.0, g = 0.0, b = 1.0, a = 1.0)
         
 
-        self.publisher_.publish(led_msg)
+        self.publisher_.publish(self.led_msg)
 
 
 def main(args=None):
